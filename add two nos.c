a14b8a606dd32ba2927a0503d8dd553dbdd9c54d
Manya-G2E42 <25121cn108@glbitm.ac.in>
2026-02-10 15:58:52 +0530

    Add files via upload

---
+++ add two nos.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("enter the nos:\n");
+    scanf("%d %d", &a, &b);
+
+    sum= a+b;
+    printf("the sum of the nos is: %d", sum);
+    return 0;
+}
